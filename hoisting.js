d4fb583fe98faa1b18fa2f68220c91df66f5532a
kelvin <muhammadfalahalfarisi@gmail.com>
2023-07-19 07:32:38 +0800

    saya menambahkan local host pada file saya

--- hoisting.js
+++ hoisting.js
@@ -1,1 +1,26 @@
 // Execution Context, Hoisting and Scope
+
+const nma = 'kelvin';
+console.log(nama);
+
+// Creation Phase Pada Global Context
+// Nama Var = undified
+// Nama Function = fn()
+// Hoisting
+// Window = Global Object
+// This = Window 
+
+// Execution Phase
+
+console.log(sayHello);
+
+var nama = 'Falah';
+var umur = 16;
+
+function sayHello() {
+    console.log(`Halo, nama saya ${nama} dan umur saya ${umur} tahun `);
+}
+
+// Function membuat local execution context
+
+
