aee2d43d0eb9b7e374e832473caf30da24117334
kelvin <muhammadfalahalfarisi@gmail.com>
2023-07-20 09:43:26 +0800

    Saya membuat paragraf baru pada oop saya

--- oop.js
+++ oop.js
@@ -6,38 +6,52 @@
 // Menambahkan property this pada const function
 
 // contoh 1
-function OrangBaru(namaDepan,namaBelakang) {
-   this.namaDepan ='';
-   this.namaBelakang = '';
-   this.sayHello() = function (nama) {
-      console.log(`Hello ${nama}, my name is ${this.namaDepan}`);
-   }
-}
+// function OrangBaru(namaDepan,namaBelakang) {
+//    this.namaDepan ='';
+//    this.namaBelakang = '';
+//    this.sayHello() = function (nama) {
+//       console.log(`Hello ${nama}, my name is ${this.namaDepan}`);
+//    }
+// }
 
-const kelvin = new OrangBaru();
-kelvin.namaDepan = 'Muhammad';
-kelvin.namaBelakang = 'Raditya';
+// const kelvin = new OrangBaru();
+// kelvin.namaDepan = 'Muhammad';
+// kelvin.namaBelakang = 'Raditya';
 
-const falah = new OrangBaru();
+// const falah = new OrangBaru();
 
 
 
-    // Constructor Inheritance
-    function Employee(firstName) {
-        this.firstName = firstName;
-        this.sayHello = function (name) {
-            console.info(`hi ${name}, my name is ${this.firstName}`);
-        }
-    };
+//     // Constructor Inheritance
+//     function Employee(firstName) {
+//         this.firstName = firstName;
+//         this.sayHello = function (name) {
+//             console.info(`hi ${name}, my name is ${this.firstName}`);
+//         }
+//     };
 
-    function Manager (firstName, lastName) {
-        this.lastName = lastName;
-        Employee.call(this, firstName);
-    };
+//     function Manager (firstName, lastName) {
+//         this.lastName = lastName;
+//         Employee.call(this, firstName);
+//     };
 
-    const thirza = new Manager('Nur', 'Zanetta');
-    console.log(thirza);
+//     const thirza = new Manager('Nur', 'Zanetta');
+//     console.log(thirza);
 
     // Prototype Inheritance
     // Object Instance
+
+    function Person (firstName,lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sayHello = funtion (name) {
+            console.info(`Hello ${name}, my name is ${this.firstName}`);
+        }
+    }
+
+    const kelvin = new Person ('Muhammad','Falah');
+    const thirza = new Person ('Nur','Zanetta');
+
+    console.log(kelvin);
+    console.log(thirza);
     
