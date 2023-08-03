d065dfa915e2f275d34fe9ec78cac4cac18d362b
kelvin <muhammadfalahalfarisi@gmail.com>
2023-08-03 09:06:12 +0800

    berhasil membuat contoh statis method

--- oop.js
+++ oop.js
@@ -302,7 +302,20 @@
   // console.info(Configuration.author);
 
   //* Static Method
-  
+  class MathUtil {
+    static sum (...numbers){
+      let total = 0;
+      for (const number of numbers) {
+          total += number;
+      }
+      return total;
+    }
+
+
+  }
+
+  const result = MathUtil.sum(1,1,1,1,1);
+  console.info(result);
 
 
 
